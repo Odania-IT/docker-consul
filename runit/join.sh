fbaacd3c3c3bbbf356602c44797905ed5b2fa580
Mike Petersen <mike@odania-it.de>
2016-03-08 11:10:23 +0100

    Allowing second join parameter

--- runit/join.sh
+++ runit/join.sh
@@ -6,4 +6,9 @@ if [ -n "$CONSULMASTER_ADDR" ]; then
 	consul join ${CONSULMASTER_ADDR}
 fi
 
+if [ -n "$CONSULMASTER_PORT_8400_TCP_ADDR" ]; then
+	echo consul join ${CONSULMASTER_PORT_8400_TCP_ADDR}
+	consul join ${CONSULMASTER_PORT_8400_TCP_ADDR}
+fi
+
 exec kill -STOP "$$";
