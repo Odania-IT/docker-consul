1479dcd34041c95d703631d6c622e2a5a3b0dc27
Mike Petersen <mike@odania-it.de>
2016-03-08 11:19:04 +0100

    Updated setting without mode

--- runit/consul.sh
+++ runit/consul.sh
@@ -5,6 +5,7 @@ else
 	if [ "${BOOTSTRAP_EXPECT,,}" == "true" ]; then
 		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -bootstrap-expect $BOOTSTRAP_EXPECT
 	else
-		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -bootstrap-expect 1
+		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0
 	fi
 fi
+gs
