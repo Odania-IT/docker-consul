56c48366e87174dee1ce2c35321d5697a89a1f42
Mike Petersen <mike@odania-it.de>
2016-03-08 12:14:20 +0100

    Allow setting of DATA_DIR

--- runit/consul.sh
+++ runit/consul.sh
@@ -4,12 +4,16 @@ if [ -z "${ADVERTISE_IP}" ]; then
 	ADVERTISE_IP=$(hostname -i)
 fi
 
+if [ -z "${DATA_DIR}" ]; then
+	DATA_DIR=/tmp/consul
+fi
+
 if [ "${BOOTSTRAP_NODE,,}" == "true" ]; then
-	exec /usr/local/bin/consul agent -bootstrap -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP
+	exec /usr/local/bin/consul agent -bootstrap -server -data-dir=${DATA_DIR} -client=0.0.0.0 -advertise=$ADVERTISE_IP
 else
 	if [ "${BOOTSTRAP_EXPECT,,}" == "true" ]; then
-		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP -bootstrap-expect $BOOTSTRAP_EXPECT
+		exec /usr/local/bin/consul agent -server -data-dir=${DATA_DIR} -client=0.0.0.0 -advertise=$ADVERTISE_IP -bootstrap-expect $BOOTSTRAP_EXPECT
 	else
-		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP
+		exec /usr/local/bin/consul agent -server -data-dir=${DATA_DIR} -client=0.0.0.0 -advertise=$ADVERTISE_IP
 	fi
 fi
