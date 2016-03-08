01906023ab64522d8ff127c7b7736fe342d19239
Mike Petersen <mike@odania-it.de>
2016-03-08 11:26:41 +0100

    Detect advertise ip if not specified

--- runit/consul.sh
+++ runit/consul.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
+
+if [ -z "${ADVERTISE_IP}" ]; then
+	ADVERTISE_IP=$(/sbin/ifconfig | sed -n '2 p' | awk '{print $3}')
+fi
+
 if [ "${BOOTSTRAP_NODE,,}" == "true" ]; then
-	exec /usr/local/bin/consul agent -bootstrap -server -data-dir=/tmp/consul -client=0.0.0.0
+	exec /usr/local/bin/consul agent -bootstrap -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP
 else
 	if [ "${BOOTSTRAP_EXPECT,,}" == "true" ]; then
-		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -bootstrap-expect $BOOTSTRAP_EXPECT
+		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP -bootstrap-expect $BOOTSTRAP_EXPECT
 	else
-		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0
+		exec /usr/local/bin/consul agent -server -data-dir=/tmp/consul -client=0.0.0.0 -advertise=$ADVERTISE_IP
 	fi
 fi
 gs
